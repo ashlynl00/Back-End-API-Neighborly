7a316551986c9f455015feca8612d402b7ffef25
Ashlyn Lanaville <a14079@MacBook-Air.local>
2022-03-28 16:29:01 -0700

    added img to model

--- models/neighborhood.js
+++ models/neighborhood.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 const neighborhoodSchema = new Schema ({
     name: {type: String, required: true},
-    location: {type: String, required: true}
+    location: {type: String, required: true},
+    img: {type: String, required: false}
 });
 
 const Neighborhood = mongoose.model('Neighborhood', neighborhoodSchema);

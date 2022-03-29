8a68d6a23f60138b3c6d5bc03c92160304d0b923
Ashlyn Lanaville <a14079@MacBook-Air.local>
2022-03-28 17:10:15 -0700

    added default img to model

--- models/neighborhood.js
+++ models/neighborhood.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const neighborhoodSchema = new Schema ({
     name: {type: String, required: true},
     location: {type: String, required: true},
-    img: {type: String, required: false}
+    img: {type: String, required: true, default: "https://i.imgur.com/wG8tIvl.jpg"}
 });
 
 const Neighborhood = mongoose.model('Neighborhood', neighborhoodSchema);
